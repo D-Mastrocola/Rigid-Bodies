f8745b7f0ecbb5800ef169e810afb9ce07ce4c79
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-05-19 13:31:58 -0400

    update

--- src/main.js
+++ src/main.js
@@ -6,15 +6,23 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let rigidBodies = [];
 
-let body1 = new RigidBody(0, canvas.height - 100, 50, "#ff0000");
-let body2 = new RigidBody(200, canvas.height - 100, 50, "#ff00ff");
+//let body1 = new RigidBody(0, canvas.height - 100, 70, "#ff0000");
+//let body2 = new RigidBody(200, canvas.height - 100, 50, "#ff00ff");
 
-rigidBodies.push(body1);
-rigidBodies.push(body2);
+let colors = ['#ff0000', '#ff9900', '#ffff00', '#99ff00', '#00ff00', '#00ff99', '#00ffff', "#0099ff", '#0000ff', "#9900ff", "#ff00ff"]
+
+
+
+
 
 let init = () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  for(let i = 0; i < 5; i++) {
+    let body = new RigidBody(Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 200 + 50, colors[Math.round(Math.random()*10)])
+    rigidBodies.push(body);
+  }
+  
   window.requestAnimationFrame(gameLoop);
 };
 
@@ -34,7 +42,7 @@ let draw = () => {
 
 let update = () => {
   for (let i = 0; i < rigidBodies.length; i++) {
-    rigidBodies[i].update(GRAVITY, canvas);
+    rigidBodies[i].update(GRAVITY, canvas, rigidBodies, i);
   }
   draw();
 };
--- src/objects/rigidBody.js
+++ src/objects/rigidBody.js
@@ -6,60 +6,94 @@ class RigidBody {
     };
     this.force = {
       x: 200,
-      y: 200
-    }
+      y: 200,
+    };
     this.velocity = {
       x: 0,
       y: 0,
     };
     this.mass = mass;
     this.centerOfMass = {
-      x: this.x - this.mass/2,
-      y: this.y - this.mass/2,
-    }
+      x: this.x - this.mass / 2,
+      y: this.y - this.mass / 2,
+    };
     this.rotation = 0;
-
+    this.collision = false;
     this.color = color;
+    console.log(color);
   }
-  checkCollisions(canvas) {
+  checkCollisions(canvas, rigidBodies, index) {
     //Out of bounds
-    if (this.position.y + this.mass/2 > canvas.height) {
-      this.position.y = canvas.height - this.mass;
+    if (this.position.y + this.mass / 2 >= canvas.height) {
+      this.position.y = canvas.height - this.mass/2;
       this.velocity.y *= -1;
     }
-    if (this.position.y < 0) {
-      this.position.y = 0;
+    if (this.position.y - this.mass / 2 <= 0) {
+      this.position.y = this.mass/2;
       this.velocity.y *= -1;
     }
-    if (this.position.x < 0) {
-      this.position.x = 0;
+    if (this.position.x - this.mass / 2 <= 0) {
+      this.position.x = this.mass/2;
       this.velocity.x *= -1;
     }
-    if (this.position.x + this.mass/2 > canvas.width) {
-      this.position.x = canvas.width - this.mass;
+    if (this.position.x + this.mass / 2 >= canvas.width) {
+      this.position.x = canvas.width - this.mass/2;
       this.velocity.x *= -1;
     }
-
 
     //Rigid Bodies
+    for (let i = 0; i < rigidBodies.length; i++) {
+      if (i === index) continue;
+      if (
+        this.position.x + this.mass/2 >
+          rigidBodies[i].position.x-rigidBodies[i].mass/2 &&
+        this.position.x - this.mass / 2 <
+          rigidBodies[i].position.x + rigidBodies[i].mass/2
+      ) {
+        if (
+          this.position.y + this.mass/2 >
+            rigidBodies[i].position.y-rigidBodies[i].mass/2 &&
+          this.position.y - this.mass / 2 <
+            rigidBodies[i].position.y + rigidBodies[i].mass/2
+        ) {
+          this.collision = true;
+        }
+      }
+    }
   }
-  update(GRAVITY, canvas) {
+  update(GRAVITY, canvas, rigidBodies, index) {
+    this.collision = false;
     this.velocity.x += this.force.x / this.mass;
     this.velocity.y += this.force.y / this.mass;
 
     this.force = {
       x: 0,
-      y: 0
+      y: 0,
     };
 
     this.velocity.y += GRAVITY;
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
-    this.checkCollisions(canvas);
+    this.checkCollisions(canvas, rigidBodies, index);
   }
   draw(ctx) {
+    
+    if(this.collision) {
+      ctx.fillStyle = '#ffffff';
+      ctx.fillRect(
+        this.position.x - this.mass / 2 - 2,
+        this.position.y - this.mass / 2 - 2,
+        this.mass + 4,
+        this.mass + 4
+      );
+    }
     ctx.fillStyle = this.color;
-    ctx.fillRect(this.position.x - this.mass/2, this.position.y - this.mass/2 , this.mass, this.mass);
+    ctx.fillRect(
+      this.position.x - this.mass / 2,
+      this.position.y - this.mass / 2,
+      this.mass,
+      this.mass
+    );
   }
 }
 export default RigidBody;
